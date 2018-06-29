07faea2a50543c8bc45461cabd8177d53f1d7380
Carl Whittick <carlwhittick@gmail.com>
2018-06-29 21:42:19 +0100

    Removed comment

--- src/auth.js
+++ src/auth.js
@@ -3,14 +3,6 @@ import store from './store';
 
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-        // const newUser = {
-        //     id: user.uid,
-        //     name: user.displayName,
-        //     email: user.email,
-        //     photoUrl: user.photoURL,
-        //     profile: firestore.collection("users").doc(user.uid)
-        // }
-        // store.commit('user/setUser', newUser)
         store.dispatch('user/load')
     } else {
         store.dispatch('user/signOut');
